372d8ba9bdb73df1b62bf3dcd908dd6fe6f92c98
dtom777 <ethan230204@gmail.com>
2021-04-06 23:35:28 +0900

    layout修正

--- components/VideoPlay.vue
+++ components/VideoPlay.vue
@@ -1,15 +1,12 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="8">
-        <youtube
-          :videoId="videoId"
-          ref="youtube"
-          :player-vars="{ autoplay: 1 }"
-        />
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="wrap">
+    <youtube
+      :videoId="videoId"
+      ref="youtube"
+      :player-vars="{ autoplay: 1 }"
+      class="video"
+    />
+  </div>
 </template>
 
 <script>
@@ -28,3 +25,22 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+.wrap {
+  position: relative;
+  width: 100%;
+  &:before {
+    display: block;
+    content: "";
+    padding-top: 56.2%;
+  }
+  .video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100% !important;
+    height: 100% !important;
+  }
+}
+</style>
